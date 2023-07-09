a75fb4f13da541a91e2b52d96c0ae891f0380811
pxulin <fudevil123@gmail.com>
2023-07-09 00:06:57 -0300

    new

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import './Home.css';
 import { Fade } from 'react-awesome-reveal';
 const Home = () => {
+  const date = new Date();
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
 
   return (
     <section id="home">
       <div className="grid-layout">
         <Fade direction='down' cascade duration={1000} triggerOnce>
-          <p>jul 09, 2003</p>
+          <p>
+            {month + '-' + day + '-' + year}</p>
           <h1>HI, MY NAME IS PAULO VITOR, I'M A FRONT-END DEVELOPER<span id="mySpan"></span></h1>
 
           <div className="button-effect">
